cdc40d65965d44c54a38adf6260797506b84afa7
mahm0ud-abdelhalim <mahmoud.abdelhalim.mostafa@outlook.com>
2022-02-06 15:36:25 +0200

    migration

---
+++ database/migrations/2022_02_06_115547_create_articles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\PseudoTypes\True_;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->string('slug')->unique();
+            $table->boolean('is_used')->default(True);
+            $table->unsignedBigInteger('cat_id');
+            $table->foreign('cat_id')
+                ->references('id')
+                ->on('categories')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
